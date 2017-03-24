ceb76b31fbc6bde11c72848f3d3e3f5ecfbfd127
ledzeppkiran <ledzeppkiran@gmail.com>
2017-03-24 10:02:16 +0530

    Update script.js

--- WP_A3_69/js/script.js
+++ WP_A3_69/js/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded",
         // Call server to get the name
         $ajaxUtils
           .sendGetRequest("data/name.json", 
-            function (res) {
+            function sayHello(res) {
               var message = 
                 res.firstName + " " + res.lastName
               if (res.likesChineseFood) {
