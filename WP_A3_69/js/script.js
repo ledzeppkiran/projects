915e701e775cb0df4f51c5e187bd3170144b6275
ledzeppkiran <ledzeppkiran@gmail.com>
2017-03-24 11:01:02 +0530

    Update script.js

--- WP_A3_69/js/script.js
+++ WP_A3_69/js/script.js
@@ -1,38 +1,40 @@
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
-    
+
     // Unobtrusive event binding
     document.querySelector("button")
-      .addEventListener("click", function () {
-        
+    .addEventListener("click", function () {
+
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name.json", 
-            function (res) {
+        .sendGetRequest("data/name.json", 
+          function (res) {
 
-              var temp =
-                document.getElementById("name").value;
+            var temp =
+            document.getElementById("name").value;
 
-                if(temp === res.name1){
+            if(temp === res.name1){
               var message = 
-                 "Course is :  " + res.course1 + 
-                  "attendance is: " + res.attendance1;
+              "Course is :  " + res.course1 + "attendance
+              is : " + res.attendance1;
               
-              }
-              else if (temp === res.name2){
-                message = "Course is :  " + res.course2 + 
-                 "attendance is : " + res.attendance2;
-                 message += "you are in defaulters"
-              }
-          
+            }
+            else if (temp === res.name2){
+              message = "Course is :  " + res.course2 + "attendance
+              is : " + res.attendance2;
+              message += "you are in defaulters"
+            }
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>" + message + "</h2>";
-            });
-      
-   }
-  );
+
+            document.querySelector("#content")
+            .innerHTML = "<h2>" + message + "</h2>";
+          });
+
+      });
+      });
+
+
 
 
  
