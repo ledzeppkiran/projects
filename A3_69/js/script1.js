4bb07c49c0ffafa79b9bdd7e4a987edf2c834e8a
ledzeppkiran <ledzeppkiran@gmail.com>
2017-04-21 10:58:58 +0530

    Add files via upload

---
+++ A3_69/js/script1.js
@@ -0,0 +1,59 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.getElementById("name")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("name1.json", 
+            function (res) {
+              var message = "";
+              var input=
+              document
+                .getElementById("in")
+                .value;
+                console.log("input value "+input);
+                var count=0;
+              for(var i=0;i<3;i++)
+              {
+                if(res[i].sapid==input)
+                {
+                  message+=res[i].name+" ";
+                  count++;
+                }
+                else if(res[i].email===input)
+                {
+                  message+=res[i].name+" ";
+                  count++;
+                }
+                else if(res[i].contactno==input)
+                {
+                  message+=res[i].name+" ";
+                  count++;
+                }
+                else if(res[i].cgpa==input)
+                {
+                  message+=res[i].name+" ";
+                  count++;
+                }
+                else if(res[i].dob===input)
+                {
+                  message+=res[i].name+" ";
+                  count+;
+                }
+
+              }
+              if(count==0)
+              {
+                message+="data not found";
+              }
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
